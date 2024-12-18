120423f86e375401350781919f5d89dbf8359578
Daril Rodriguez <darilrodriguez.2@gmail.com>
2024-12-18 00:36:51 -0300

    add events to app

--- include/amuse_ecs/app.hpp
+++ include/amuse_ecs/app.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "amuse_ecs/world.hpp"
+#include "amuse_ecs/event_handler.hpp"
 
 namespace ecs
 {
@@ -9,9 +10,58 @@ namespace ecs
     EVENT(Update);
     EVENT(PostUpdate);
 
+    class App;
+
+    template <typename... Components>
+    class SystemBuilder
+    {
+        App &_app;
+        World &_world;
+        EventHandler &_event_handler;
+
+    public:
+        SystemBuilder(App &app, World &world, EventHandler &event_handler)
+            : _app(app), _world(world), _event_handler(event_handler)
+        {
+        }
+
+        template <typename Event>
+        SystemBuilder<Components...> &on([[maybe_unused]] const std::function<void(App &, Components &...)> &&func)
+        {
+            if constexpr (std::is_same_v<Event, Set>)
+            {
+                _world.on<Set>(
+                    _world.get_archetype_id<Components...>(),
+                    [func = std::move(func), *this]()
+                    {
+                        _world.view<Components...>().each(
+                            [func, *this](Components &...components)
+                            {
+                                func(_app, components...);
+                            });
+                    });
+            }
+            else
+            {
+                _event_handler.on<Event>(
+                    [func = std::move(func), *this]()
+                    {
+                        _world.view<Components...>().each(
+                            [func, *this](Components &...components)
+                            {
+                                func(_app, components...);
+                            });
+                    });
+            }
+
+            return *this;
+        }
+    };
+
     class App
     {
         World _world;
+        EventHandler _event_handler;
 
     public:
         template <typename Module>
@@ -26,6 +76,11 @@ namespace ecs
             return _world;
         }
 
+        inline EventHandler &event_handler()
+        {
+            return _event_handler;
+        }
+
         void run();
 
         template <typename Component>
@@ -35,9 +90,9 @@ namespace ecs
         }
 
         template <typename... Components>
-        void system()
+        SystemBuilder<Components...> system()
         {
-            // TODO: Implement system
+            return SystemBuilder<Components...>(*this, _world, _event_handler);
         }
     };
 } // namespace ecs
--- tests/app.cpp
+++ tests/app.cpp
@@ -9,14 +9,6 @@ struct ModuleA
     {
         app.register_component<Position>();
         app.register_component<Velocity>();
-
-        // app.system<Position>()
-        //     .on<ecs::Set>(
-        //         []()
-        //         {
-        //             p.x = 1.0f;
-        //             p.y = 2.0f;
-        //         });
     }
 };
 
@@ -28,4 +20,31 @@ TEST(AppImportModule, "Import module")
     auto &world = app.world();
 
     ASSERT(world.component_deleters.size() == 2);
+}
+
+TEST(AppEvent, "Event handling")
+{
+    ecs::App app;
+    app.use<ModuleA>();
+
+    bool called_position_velocity_on_set = false;
+    bool called_position_velocity_on_update = false;
+
+    app.system<Position, Velocity>()
+        .on<ecs::Set>([&]([[maybe_unused]] ecs::App &app, [[maybe_unused]] Position &p, [[maybe_unused]] Velocity &v)
+                      { called_position_velocity_on_set = true; })
+        .on<ecs::Update>(
+            [&]([[maybe_unused]] ecs::App &app, [[maybe_unused]] Position &p, [[maybe_unused]] Velocity &v)
+            {
+                called_position_velocity_on_update = true;
+            });
+
+    app.world().entity().add<Position>({1.0f, 2.0f}).add<Velocity>({3.0f, 4.0f});
+
+    app.world().dispatch();
+
+    app.event_handler().emit<ecs::Update>();
+
+    ASSERT(called_position_velocity_on_set);
+    ASSERT(called_position_velocity_on_update);
 }
